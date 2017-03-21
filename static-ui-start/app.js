7893e000cd968475c15a3687e02645b9ff043dc8
Charles <charles.m.long43@gmail.com>
2017-03-20 20:08:00 -0500

    last Fridays changes

---
+++ static-ui-start/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const chalk = require('chalk');
+const nunjucks = require('nunjucks');
+const router=require('./routes');
+
+const db = require('./db').db;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan('dev'));
+
+nunjucks.configure('views', { noCache: true });
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+
+app.use(express.static('public'));
+app.use('/bootstrap', express.static('./node_modules/bootstrap/dist'));
+app.use('/jquery', express.static('./node_modules/jquery/dist'));
+app.use('/',router);
+
+// root path
+app.get('/', function(req, res, next) {
+  res.render('index', {message: 'hello'});
+});
+
+// error handling
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500)
+    .render('error', {message: 'oh no :-('});
+});
+
+//{force: true}
+db.sync()
+  .then(() => {
+    console.log(chalk.cyan('DB SYNCED!'));
+    app.listen(1337, () => {
+      console.log(chalk.green('Planning on port 1337'));
+    });
+  })
+  .catch(err => {
+    console.log(chalk.red('DB SYNC WENT WRONG...'));
+  });
---
+++ static-ui-start/routes/index.js
@@ -0,0 +1,33 @@
+const router=require('express').Router();
+const { db, Place, Hotel, Restaurant, Activity } = require('../db');
+
+
+let dbholder={};
+
+router.get('/',function(req,res,next){
+  // Place.findAll({}).then(function(results){
+  //     // console.log(results[0].toJSON());
+  //     return res.render('index', {results: results});
+  // })
+  Hotel.findAll({})//*** don't forget to pass in empty object
+    .then(function(hotelArr){
+      dbholder.hotels=hotelArr;
+      return Restaurant.findAll({});
+    })
+    .then(restaurantArr=>{
+      dbholder.restaurants=restaurantArr;
+      return Activity.findAll({});
+    })
+    .then(activityArr=>{
+      dbholder.activities=activityArr;
+      res.render('index', {
+        templateHotels: dbholder.hotels,
+        templateRestaurants: dbholder.restaurants,
+        templateActivities: dbholder.activities
+      })
+    })
+    .catch(err=>console.log(err));
+})
+
+
+module.exports=router
